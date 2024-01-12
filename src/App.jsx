ba0f0ce3f9a5a89f0a38c24d977190b2d70c2aaa
Yaksh Chopra <yaksh@Yakshs-MacBook-Pro.local>
2024-01-12 18:03:01 +0530

    chore: added a 2d view

--- src/App.jsx
+++ src/App.jsx
@@ -1,158 +1,159 @@
-/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
-/* eslint-disable react/display-name */
+/* eslint-disable react/no-unknown-property */
+// src/App.jsx
+
 import * as THREE from 'three';
-import { memo, useRef, useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import {
   Grid,
-  AccumulativeShadows,
-  RandomizedLight,
   Environment,
   useGLTF,
   Plane,
   OrbitControls,
 } from '@react-three/drei';
 import gsap from 'gsap';
-
-const SCALE = 10;
+import { createSeatMap } from './utils';
 
 export default function App() {
   return (
-    <Canvas
-      shadows
-      style={{ height: '100vh', width: '100vw' }}
-      camera={{ fov: 60 }}
-    >
+    <Canvas shadows style={{ height: '100vh', width: '100vw' }}>
       <Scene />
     </Canvas>
   );
 }
 
 function Scene() {
-  const meshRef = useRef();
-  const cameraControlsRef = useRef();
-
   const { camera } = useThree();
-
   const [apiData, setApiData] = useState(null);
   const [seatMapData, setSeatMapData] = useState(null);
   const [screenDimensions, setScreenDimensions] = useState(null);
+  const [view, setView] = useState('2d');
 
   useEffect(() => {
-    fetch(
-      'https://spdp.londontheatredirect.com/GetSeatingPlanScheme.ashx?locale=en-GB&performanceId=658847'
-    )
-      .then((res) => res.json())
-      .then((data) => setApiData(data))
-      .catch((err) => console.log(err));
-  }, []);
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          'https://spdp.londontheatredirect.com/GetSeatingPlanScheme.ashx?locale=en-GB&performanceId=658847'
+        );
+        const data = await response.json();
+        setApiData(data);
+        camera.near = 0.1;
+        camera.far = 3000;
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [camera]);
 
   useEffect(() => {
-    if (!apiData || !camera) return;
-    const categoryMap = createSeatMap(apiData.Seats);
-    setSeatMapData(categoryMap);
-  }, [apiData]);
+    if (apiData && camera) {
+      const categoryMap = createSeatMap(apiData.Seats);
+      setScreenDimensions(apiData.Shapes[0]);
+      setSeatMapData(categoryMap);
+    }
+  }, [apiData, camera]);
 
   useEffect(() => {
-    camera.near = 0.1;
-    camera.far = 3000;
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.position.set(0, 100, 400);
-    camera.updateProjectionMatrix();
-  }, []);
+    if (camera) {
+      if (view === '2d') {
+        camera.fov = 90;
+        camera.position.set(-50, -100, 1000);
+      } else {
+        camera.fov = 60;
+        camera.position.set(0, 100, 400);
+      }
+      camera.updateProjectionMatrix();
+    }
+  }, [view, camera]);
 
   useFrame(() => {
-    camera.lookAt(
-      -apiData.Shapes[0].Y,
-      apiData.Shapes[0].Height / 2,
-      -apiData.Shapes[0].X
-    );
+    if (screenDimensions && view !== '2d') {
+      camera.lookAt(
+        -screenDimensions.Y,
+        screenDimensions.Height / 2,
+        -screenDimensions.X
+      );
+    }
   });
 
   return (
-    <>
-      <group rotation={[0, 0, 0]} positionY={-0.5}>
-        <Ground />
-        {/* <Shadows /> */}
-        <ambientLight intensity={0.25} />
-        <directionalLight
-          castShadow
-          intensity={2}
-          position={[10, 6, -6]}
-          shadow-mapSize={[1024, 1024]}
-        >
-          <orthographicCamera
-            attach='shadow-camera'
-            left={-20}
-            right={20}
-            top={20}
-            bottom={-20}
-          />
-        </directionalLight>
-        <Environment preset='dawn' />
-        {apiData && (
-          <Screen
-            args={[apiData.Shapes[0].Width, apiData.Shapes[0].Height]}
-            x={-apiData.Shapes[0].Y}
-            y={apiData.Shapes[0].Height / 2}
-            z={-apiData.Shapes[0].X}
-            rotation={[0, 0, 0]}
-          />
-        )}
-        {apiData &&
-          seatMapData &&
-          Object.keys(seatMapData).map((item, index) => {
-            return (
-              <RenderSection
-                key={index}
-                plane={[apiData.Width, apiData.Height]}
-                seatMapData={seatMapData}
-                section={item}
-                z={(Object.keys(seatMapData).length - 1 - index) * 50}
-                camera={camera}
-                setPosition={(x, y, z) => {
-                  gsap.to(camera.position, {
-                    x: () => -x,
-                    y: () => z + 10,
-                    z: () => -y,
-                    duration: 1.5,
-                  });
-                  // camera.position.set(-x, 10, -y);
-                }}
-              />
-            );
-          })}
-        {/* {apiData && seatMapData && (
+    <group rotation={[view === '2d' ? Math.PI / 2 : 0, 0, 0]} positionY={-0.5}>
+      <Ground />
+      <ambientLight intensity={0.25} />
+      <directionalLight
+        castShadow
+        intensity={2}
+        position={[10, 6, -6]}
+        shadow-mapSize={[1024, 1024]}
+      >
+        <orthographicCamera
+          attach='shadow-camera'
+          left={-20}
+          right={20}
+          top={20}
+          bottom={-20}
+        />
+      </directionalLight>
+      <Environment preset='dawn' />
+      {apiData && screenDimensions && (
+        <Screen
+          args={[screenDimensions.Width, screenDimensions.Height]}
+          x={-screenDimensions.Y}
+          y={screenDimensions.Height / 2}
+          z={-screenDimensions.X}
+          rotation={[0, 0, 0]}
+        />
+      )}
+      {apiData &&
+        seatMapData &&
+        Object.keys(seatMapData).map((item, index) => (
           <RenderSection
-            plane={[apiData.Width, apiData.Height]}
+            key={index}
+            theatreDimensions={{ height: apiData.Width, width: apiData.Height }}
+            setView={setView}
             seatMapData={seatMapData}
-            section={'Stalls'}
-            z={0}
-            camera={camera}
+            sectionName={item}
+            sectionElevation={
+              (Object.keys(seatMapData).length - 1 - index) * 50
+            }
             setPosition={(x, y, z) => {
-              camera.position.set(-x, 10, -y);
+              gsap.to(camera.position, {
+                x: () => -x,
+                y: () => z + 10,
+                z: () => -y,
+                duration: 1,
+              });
             }}
           />
-        )} */}
-        <OrbitControls />
-      </group>
-    </>
+        ))}
+      {view !== '2d' && (
+        <OrbitControls
+          enablePan={true}
+          enableZoom={true}
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI / 2.2}
+          rotateSpeed={4}
+          panSpeed={2}
+          screenSpacePanning={false}
+        />
+      )}
+    </group>
   );
 }
 
-const Screen = (props) => {
-  return (
-    <Plane
-      position={[props.x, props.y, props.z]}
-      {...props}
-      // rotation={[Math.PI / 2, 0, 0]}
-      // args={[props.width, dimensions.Height]}
-    >
-      <meshBasicMaterial attach='material' color={'white'} />
-    </Plane>
-  );
-};
+const Screen = (props) => (
+  <Plane position={[props.x, props.y, props.z]} {...props}>
+    <meshBasicMaterial
+      attach='material'
+      color={'white'}
+      side={THREE.DoubleSide}
+    />
+  </Plane>
+);
 
 function Ground() {
   const gridConfig = {
@@ -170,63 +171,38 @@ function Ground() {
   return <Grid position={[0, 0, 0]} args={[1000, 1000]} {...gridConfig} />;
 }
 
-const Shadows = memo(() => (
-  <AccumulativeShadows
-    temporal
-    frames={100}
-    color='#9d4b4b'
-    colorBlend={0.5}
-    alphaTest={0.9}
-    scale={20}
-  >
-    <RandomizedLight amount={8} radius={4} position={[5, 5, -10]} />
-  </AccumulativeShadows>
-));
-
-function createSeatMap(seats) {
-  const seatMap = {};
-  seats.forEach((seat) => {
-    const section = seat.Iid.split('-')[0];
-    const row = seat.Row;
-
-    if (!seatMap[section]) {
-      seatMap[section] = {};
-    }
-    if (!seatMap[section][row]) {
-      seatMap[section][row] = [];
-    }
-
-    seatMap[section][row].push(seat);
-  });
-
-  return seatMap;
-}
-
-const Model = ({ color, x, y, z, size = 10, camera, setPosition, parentZ }) => {
+const Model = ({
+  seatColor,
+  x,
+  y,
+  size = 15,
+  setPosition,
+  sectionElevation,
+  setView,
+}) => {
   const gltf = useGLTF('/seat.glb');
   const modelScene = useMemo(() => {
     const clone = gltf.scene.clone();
     clone.traverse((child) => {
       if (child.isMesh) {
-        child.material.color.set(color);
+        child.material.color.set(seatColor);
       }
     });
     return clone;
-  }, [gltf.scene, color]);
+  }, [gltf.scene, seatColor]);
 
   const handleOnClick = () => {
-    console.log('test', camera.position);
-    setPosition(x, y, z);
-    // camera.lookAt(new THREE.Vector3(0, 562.37, 0));
+    setPosition(x, y, sectionElevation + 5);
+    setView('3d');
   };
 
   return (
     <primitive
       rotation={[0, Math.PI, 0]}
       onClick={handleOnClick}
-      color={color}
+      color={seatColor}
       object={modelScene}
-      position={[x, parentZ + 5, y]}
+      position={[x, sectionElevation + 5, y]}
       scale={size}
     />
   );
@@ -236,56 +212,50 @@ const RenderRow = ({
   seatMapData,
   section,
   row,
-  plane,
-  camera,
-  zOffset,
+  theatreDimensions,
   setPosition,
-  parentZ,
+  sectionElevation,
+  setView,
 }) => {
   const seats = seatMapData[section][row];
-  return seats.map((seat, index) => {
-    return (
-      <Model
-        camera={camera}
-        key={index}
-        color='grey'
-        rotation={[0, Math.PI, 0]}
-        y={plane[1] / 2 - seat.Y}
-        x={plane[0] / 2 - seat.X}
-        z={parentZ}
-        setPosition={setPosition}
-        parentZ={parentZ}
-      />
-    );
-  });
+  return seats.map((seat, index) => (
+    <Model
+      key={index}
+      setView={setView}
+      seatColor='grey'
+      rotation={[0, Math.PI, 0]}
+      y={theatreDimensions.height / 2 - seat.Y}
+      x={theatreDimensions.width / 2 - seat.X}
+      setPosition={setPosition}
+      sectionElevation={sectionElevation}
+    />
+  ));
 };
 
 const RenderSection = ({
   seatMapData,
-  section,
-  plane,
-  z,
-  camera,
+  sectionName,
+  theatreDimensions,
+  sectionElevation,
   setPosition,
+  setView,
 }) => {
-  const sectionData = seatMapData[section];
+  const sectionData = seatMapData[sectionName];
   return (
-    <group position={[0, 0, 0]} rotation={[0, Math.PI, 0]}>
-      {Object.keys(sectionData).map((row, index) => {
-        return (
-          <RenderRow
-            camera={camera}
-            key={index}
-            seatMapData={seatMapData}
-            section={section}
-            row={row}
-            zOffset={index}
-            plane={plane}
-            setPosition={setPosition}
-            parentZ={z}
-          />
-        );
-      })}
+    <group rotation={[0, Math.PI, 0]}>
+      {Object.keys(sectionData).map((row, index) => (
+        <RenderRow
+          key={index}
+          seatMapData={seatMapData}
+          section={sectionName}
+          row={row}
+          setView={setView}
+          theatreDimensions={theatreDimensions}
+          setPosition={setPosition}
+          sectionElevation={sectionElevation}
+          zOffset={index}
+        />
+      ))}
     </group>
   );
 };
